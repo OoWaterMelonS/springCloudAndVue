9b40d61f2472b61a65ce877c869ab9f11060ab0f
OoWaterMelons <15884455616@163.com>
2020-06-03 19:56:15 +0800

    1 分页参数前后端交互，axios的post请求默认是以流的方式传递参数，所以controller里的参数要加@RequestBody注解 2 jquery默认使用的表单的方式，后端默认也是使用的表单接收方式

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,8 +3,10 @@ package com.course.server.service;
 import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
+import com.course.server.dto.PageDto;
 import com.course.server.mapper.ChapterMapper;
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +24,16 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<ChapterDto> list(){
+    // 为什么不返回了呢
+    public PageDto list(PageDto pageDto){
         // 查第一页，查一条
         // 注意：pageNum是从1开始的，不是从0开始的
-        PageHelper.startPage(2,1);
+        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
 
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
+        PageInfo<Chapter> pageInfo = new PageInfo<>(chapterList);
+        pageDto.setTotal(pageInfo.getTotal());
         List<ChapterDto> chapterDtoList = new ArrayList<ChapterDto>();
         for ( int i = 0,length = chapterList.size(); i < length; i++){
             ChapterDto chapterDto = new ChapterDto();
@@ -38,6 +43,8 @@ public class ChapterService {
                 chapterDtoList.add(chapterDto);
             }
         }
-        return chapterDtoList;
+        pageDto.setList(chapterList);
+        // 此处的pageDto是从前端拿到的  后端不返回，前端也能拿到这个对象
+        return pageDto;
     }
 }
