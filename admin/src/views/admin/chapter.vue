c8b75dbaca4976d713a86fabe98c8618dc84d541
OoWaterMelons <15884455616@163.com>
2020-06-05 20:52:59 +0800

    1 集成sweetalert2,删除时弹出确认框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -170,10 +170,29 @@
       },
       del(id){
         let _this = this;
-        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
-          console.log("删除章列表结果", response);// chapter作为前后端交互传参
-          _this.list(1);//删除成功以后 重新查询一遍即可
+        Swal.fire({
+          title: '确认删除',
+          text: "删除后不可恢复，确认删除!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: '确认!'
+        }).then((result) => {
+          if (result.value) {
+            _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
+              console.log("删除章列表结果", response);// chapter作为前后端交互传参
+              _this.list(1);//删除成功以后 重新查询一遍即可
+              Swal.fire(
+                '删除成功!',
+                '删除成功!',
+                'success'
+              )
+            });
+
+          }
         });
+
 
       },
     }
