ba035b42ea4426fac733fcbd14fbc29304b487f7
OoWaterMelons <15884455616@163.com>
2020-06-07 22:49:01 +0800

    1 小节表section表结构设计 2 生成section表持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -31,3 +31,40 @@ create table `test` (
 
 insert into `test` (id, name) values (1, '测试');
 insert into `test` (id, name) values (2, '测试2');
+
+
+# 小节表
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section`(
+    `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+    `title` VARCHAR(50) NOT NULL COMMENT '标题',
+    `course_id` CHAR(8) COMMENT '课程|course.id',
+    `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+    `video` VARCHAR(200) COMMENT '视频',
+    `time` INT COMMENT '时长|单位秒',
+    `charge` CHAR(1) COMMENT '收费|C 收费;F 免费',
+    `sort` INT COMMENT '顺序',
+    `created_at` DATETIME COMMENT '创建时间',
+    `updated_at` DATETIME COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT ='小节';
+
+INSERT INTO `section` (id,title,course_id,chapter_id,video,time,charge,sort,created_at,updated_at)
+VALUES ('00000001','测试小节01','00000001','00000000','',500,'F',1,now(),now());
+
+
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section`(
+                          `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+                          `title` VARCHAR(50) NOT NULL COMMENT '标题',
+                          `course_id` CHAR(8) COMMENT '课程|course.id',
+                          `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+                          `video` VARCHAR(200) COMMENT '视频',
+                          `time` INT COMMENT '时长|单位秒',
+                          `charge` CHAR(1) COMMENT '收费|C 收费;F 免费',
+                          `sort` INT COMMENT '顺序',
+                          PRIMARY KEY (`id`)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT ='小节';
+
+INSERT INTO `section` (id,title,course_id,chapter_id,video,time,charge,sort)
+VALUES ('00000001','测试小节01','00000001','00000000','',500,'F',1);
