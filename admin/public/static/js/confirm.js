7bf36620c6ffbcf7703967c42180b87439131b13
OoWaterMelons <15884455616@163.com>
2020-06-07 09:09:32 +0800

    1 增加工具类tool.js和校验类validator.js 2 大章保存非空和长度增加校验

--- admin/public/static/js/confirm.js
+++ admin/public/static/js/confirm.js
@@ -9,6 +9,7 @@ Confirm = {
       cancelButtonColor: '#d33',
       confirmButtonText: '确认!'
     }).then((result) => {
+      //个人理解 当结果值 是真的情况  才去执行需要的函数
       if (result.value) {// 将变化的代码（组件无关的代码）作为回调函数传递进来
         if (callback) {
           callback()
