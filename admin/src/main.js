2d4aaf45a60585b872b6fd6910a60ba384c13684
OoWaterMelons <15884455616@163.com>
2020-06-10 21:31:10 +0800

    1 增加vue多环境配置，后端地址改为读环境变量

--- admin/src/main.js
+++ admin/src/main.js
@@ -20,3 +20,6 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
+
+console.log(process.env.NODE_ENV);
--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -144,7 +144,7 @@
       list(page){
         let _this = this;
         Loading.show();
-        _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
+        _this.$ajax.post(process.env.VUE_APP_SERVER+'/business/admin/chapter/list',{
           page: page,
           size: _this.$refs.pagination.size
         }).then((response)=>{
@@ -167,7 +167,7 @@
         }
 
         Loading.show();
-        _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',_this.chapter).then((response)=>{
+        _this.$ajax.post(process.env.VUE_APP_SERVER+'/business/admin/chapter/save',_this.chapter).then((response)=>{
           Loading.hide();
           let resp = response.data;
           if(resp.success){
@@ -184,7 +184,7 @@
         let _this = this;
         Confirm.show('删除大章后不可恢复，确认删除？',function () {
           Loading.show();
-          _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response) => {
+          _this.$ajax.delete(process.env.VUE_APP_SERVER+'/business/admin/chapter/delete/' + id).then((response) => {
             Loading.hide();
             _this.list(1);//删除成功以后 重新查询一遍即可
             Toast.success("删除成功！");
